67744a36ad9adb015b1fbe21f688c26b78704420
Leticipandora <82900002+Leticipandora@users.noreply.github.com>
2021-05-10 17:11:57 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int número1;
+        int number1;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um número");
+        number1 = scanner.nextInt();
+
+        System.out.println("O número informado foi " + number1);
+    }
+}
